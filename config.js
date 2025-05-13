570a9d338d4e6e87df7d5ee316ced455c7bc200a
Olexander <maliy.olexander@lll.kpi.ua>
2025-05-13 20:20:56 +0300

    added config without private data

---
+++ config.js
@@ -0,0 +1,25 @@
+export default {
+    account: {
+        login: 'your_login',
+        password: 'your_password'
+    },
+    person: {
+        firstName: 'John',
+        lastName: 'Doe',
+        pesel: '01234567890',
+        ppk: '123456789',
+        category: 'b',
+        email: 'john.doe@example.com',
+        phone: '+15551234567',
+        word: {
+            cssNumber: 56
+        }
+    },
+    daysAhead: 14,
+    telegram: {
+        botToken: 'your_telegram_bot_token_here',
+        chatId: 'your_telegram_chat_id_here'
+    },
+    notifyVia: 'telegram',
+    refreshTime: 12
+};
